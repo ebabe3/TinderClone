d69a9fc6e26748a9f419915c548c801415fbfae9
Ebru Boyacı <ebruboyaci35@gmail.com>
2022-07-01 22:36:41 +0300

    add react native reanimated

--- src/pages/DashboardScreen.js
+++ src/pages/DashboardScreen.js
@@ -1,15 +1,28 @@
 import * as React from 'react' 
+
+import 'react-native-gesture-handler';
 import { StyleSheet, View } from 'react-native';
+import Animated, { useAnimatedStyle, useSharedValue } from 'react-native-reanimated';
 
 import Card from '../components/Card'
 import users from '../assets/data/users'
 
-
 const Dashboard = () => {
+
+  const sharedValue = useSharedValue(1);
+
+  const cardStyle = useAnimatedStyle(
+    () => ({
+      opacity: sharedValue.value,
+    })
+  )
+
   return (
-        <View style={styles.container}>
-          <Card user={users[0]}/>
-        </View>
+    <View style={styles.container}>
+      <Animated.View style={[styles.animatedCard,cardStyle]}>
+        <Card user={users[2]}/>
+      </Animated.View>
+    </View>
   );
 };
 
@@ -19,6 +32,12 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center"
+  },
+  animatedCard: {
+    width: '100%',
+    height: '100%',
+    justifyContent:'center',
+    alignItems: 'center',
   }
 });
 
